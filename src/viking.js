38dfee99aa8b7e34ae0b9c684106139215f2b0b1
Leonardo Araujo <leo.san9@hotmail.com>
2022-03-28 18:59:35 -0300

    saxon

--- src/viking.js
+++ src/viking.js
@@ -1,14 +1,62 @@
 // Soldier
-class Soldier {}
-
+class Soldier {
+  constructor (health, strength){
+    this.health = health;
+    this.strength = strength
+  }
+  attack(){
+    return this.strength
+  }
+  receiveDamage(damage){
+    this.health - this.damage
+  }
+}
 // Viking
-class Viking {}
+class Viking{
+  constructor (name, health, strength){
+    this.name = name;
+    this.health = health;
+    this.strength = strength;
+  }
+  attack(){
+    return this.strength
+  };
+  receiveDamage(damage){
+    if (this.health - damage > 0){
+      return `${this.name} has received ${damage} points of damage`
+    } else {return `${this.name} has died in act of combat`}
+  }
+  battleCry(){
+    return "Odin Owns You All!"
+  }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  receiveDamage(damage){
+    this.health + this.damage
+    if (this.health - damage >= 1){
+      return `A Saxon has received ${damage} points of damage`
+    } else {return "A Saxon has died in combat"}
+  }
+}
 
 // War
-class War {}
+let vikings = []
+let saxons = new Saxon(1000, 100)
+class War {
+  addViking(Viking){
+    this.addViking = vikings.push(Viking)
+  }
+  addSaxon(Saxon) {}
+  
+  vikingAttack(){
+  }
+  saxonAttack(){
+  }
+  showStatus(){
+  }
+}
 
 
 
