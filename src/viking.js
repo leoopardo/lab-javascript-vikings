d673abbd32c4ac4770410d8b617dc57c0f3873db
Leonardo Araujo <leo.san9@hotmail.com>
2022-03-29 01:45:52 -0300

    quase

--- src/viking.js
+++ src/viking.js
@@ -2,15 +2,16 @@
 class Soldier {
   constructor (health, strength){
     this.health = health;
-    this.strength = strength
+    this.strength = strength;
   }
   attack(){
     return this.strength
   }
-  receiveDamage(damage){
-    this.health - this.damage
+  receiveDamage(damage){ 
+    this.health = this.health - damage
   }
 }
+
 // Viking
 class Viking{
   constructor (name, health, strength){
@@ -22,6 +23,7 @@ class Viking{
     return this.strength
   };
   receiveDamage(damage){
+    this.health = this.health - damage;
     if (this.health - damage > 0){
       return `${this.name} has received ${damage} points of damage`
     } else {return `${this.name} has died in act of combat`}
@@ -34,23 +36,28 @@ class Viking{
 // Saxon
 class Saxon extends Soldier {
   receiveDamage(damage){
-    this.health + this.damage
-    if (this.health - damage >= 1){
+    this.health = this.health - damage;
+    if (this.health - damage > 0){
       return `A Saxon has received ${damage} points of damage`
-    } else {return "A Saxon has died in combat"}
+    } else {return "A Saxon has died in combat"};
   }
 }
 
 // War
-let vikings = []
-let saxons = new Saxon(1000, 100)
+
 class War {
-  addViking(Viking){
-    this.addViking = vikings.push(Viking)
+  constructor(){
+    this.vikingArmy = [];
+    this.saxonArmy  = [];
   }
-  addSaxon(Saxon) {}
-  
+  addViking(Viking){
+    this.vikingArmy.push(Viking)
+  }
+  addSaxon(Saxon) {
+    this.saxonArmy.push(Saxon)
+  }
   vikingAttack(){
+    Saxon.damage = Viking.strength
   }
   saxonAttack(){
   }
